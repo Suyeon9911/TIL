da1116130b8a1d2d247946d73e6403579482a9e0
suyeon <rlatndus9911@naver.com>
2022-10-11 18:09:00 +0900

    20221011 - CleanArchitectrue 연습 폴더링

---
+++ CleanArchitecture_MVVM/CleanArchitecture_MVVMUITests/CleanArchitecture_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CleanArchitecture_MVVMUITestsLaunchTests.swift
+//  CleanArchitecture_MVVMUITests
+//
+//  Created by 김수연 on 2022/10/11.
+//
+
+import XCTest
+
+final class CleanArchitecture_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
